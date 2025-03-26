27523dba795f732d72bf5341fc4195b6f01c1017
bumayy <ariyanadewa1009@gmail.com>
2025-03-26 19:58:15 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,67 @@
+function createLove() {
+  const love = document.createElement("div");
+  love.classList.add("love");
+  love.textContent = "❤";
+  love.style.left = Math.random() * window.innerWidth + "px";
+  love.style.fontSize = Math.random() * 45 + 15 + "px";
+  document.querySelector(".container").appendChild(love);
+
+  setTimeout(() => {
+    love.remove();
+  }, 5000);
+}
+
+setInterval(createLove, 300);
+
+function updateTimer() {
+  const startDate = new Date("2024-05-21T08:43:00+07:00");
+  const now = new Date();
+  const diff = now - startDate;
+
+  const days = Math.floor(diff / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((diff / (1000 * 60 * 60)) % 24);
+  const minutes = Math.floor((diff / (1000 * 60)) % 60);
+  const seconds = Math.floor((diff / 1000) % 60);
+
+  document.getElementById("days").innerText = days;
+  document.getElementById("hours").innerText = String(hours).padStart(2, "0");
+  document.getElementById("minutes").innerText = String(minutes).padStart(
+    2,
+    "0"
+  );
+  document.getElementById("seconds").innerText = String(seconds).padStart(
+    2,
+    "0"
+  );
+}
+
+setInterval(updateTimer, 1000);
+updateTimer();
+
+document.addEventListener("DOMContentLoaded", () => {
+  const readBtn = document.querySelector(".sub-btn1");
+  const popup = document.getElementById("popup");
+  const closeBtn = document.querySelector(".close-btn");
+
+  readBtn.addEventListener("click", () => {
+    popup.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+});
+
+document.addEventListener("DOMContentLoaded", () => {
+  const readBtn = document.querySelector(".main-btn");
+  const popup = document.getElementById("popup2");
+  const closeBtn = document.querySelector(".close-btn2");
+
+  readBtn.addEventListener("click", () => {
+    popup.style.display = "block";
+  });
+
+  closeBtn.addEventListener("click", () => {
+    popup.style.display = "none";
+  });
+});
